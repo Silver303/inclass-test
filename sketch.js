865664b877710c5bea0cbfc91dd4eb6c3ab565ec
Silver303 <aghadimi94@gmail.com>
2026-09-16 18:41:58 -0700

    orangeCircleTest

---
+++ sketch.js
@@ -0,0 +1,11 @@
+console.log("I believe that I can learn this.");
+
+function setup() {
+  createCanvas(800, 600);
+}
+
+function draw() {
+  background(20);
+  fill(255, 120, 60);
+  circle(mouseX, mouseY, 100);
+}
