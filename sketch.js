f7c2411e0e94d6c7c9fba3cb3a815b9968adcff7
Silver303 <aghadimi94@gmail.com>
2026-09-17 14:14:32 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,5 +7,13 @@ function setup() {
 function draw() {
   background(20);
   fill(255, 120, 60);
-  circle(mouseX, mouseY, 100);
+  circle(mouseX, mouseY, 50);
+  fill(255, 0, 60);
+  circle(400, 200, 100);
+  circle(500, 400, 200);
+  fill(90, 0, 300);
+  rect(100, 250, 200, 300);
+  fill(150, 30, 100);
+  rect(20, 50, 700, 10);
+  rect(20, 50, 10, 500);
 }
